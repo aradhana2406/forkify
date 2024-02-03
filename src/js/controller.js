c1b7dff96179f0296e5b8be5b5002e9f9dd43ded
aradhana2406 <aradhana.shinde1@gmail.com>
2024-02-03 17:29:59 +0530

    forth  commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -104,7 +104,7 @@ const init = () => {
   paginationView.addHandlerClick(controlPagination);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome here mom');
+  console.log('hey momo');
 };
 
 init();
